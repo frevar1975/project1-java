202b55d133c5864551f10207499842867cfb1649
frevar1975 <55697584+frevar1975@users.noreply.github.com>
2022-11-23 23:01:57 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,21 @@ public class Main {
         int resultant = 0;
         resultant= suma (3,8,3);
         System.out.println(resultant);
+        Coche Micoche = new Coche();
+        Micoche.addpuerta(1);
+        
     }
     public static int suma(int a, int b, int c)
     {
         return a+b+c;
     }
-    }
+}
+class Coche{
+    public int puertas = 4;
+    public void addpuerta(int nuevapuerta)
+    {
+        this.puertas= puertas + nuevapuerta;
+        System.out.println(puertas);
+    };
+   
+}
