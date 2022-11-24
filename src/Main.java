d7ce230c2aae4488e462be9af6f801481f1f69e4
frevar1975 <55697584+frevar1975@users.noreply.github.com>
2022-11-23 21:52:13 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int resultant = 0;
+        resultant= suma (3,8,3);
+        System.out.println(resultant);
     }
-}
+    public static int suma(int a, int b, int c)
+    {
+        return a+b+c;
+    }
+    }
